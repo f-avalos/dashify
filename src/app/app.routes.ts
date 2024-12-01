0093ae6864cd47a84080036f8ed8547cde944102
Jorge <jorgexm99@gmail.com>
2024-12-01 19:54:05 -0300

    initial: subida de proyecto base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', loadChildren: () => import('./features/login/login.routes').then(m => m.routes)
+  },
+  {
+    path: 'home', loadChildren: () => import('./features/home/home.routes').then(m => m.routes)
+  }
+];
